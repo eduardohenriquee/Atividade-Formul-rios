7cfab126f06aac33e0e78e564817840a85723768
eduardohenriquee <48135920+eduardohenriquee@users.noreply.github.com>
2019-07-02 10:15:22 -0300

    Create exerciocio2.php

---
+++ exerciocio2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Exercício 2</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+<div class="quadrado">
+<h1> Número para ser demonstrada a tabuada: </h1>
+<form>
+    <p>Número: <input type="number" name="numero"></p>
+    <button type="submit" style="border-radius:5px;"> Número escolhido! </button>
+</form>
+</div>
+
+</body>
+
+</html>
