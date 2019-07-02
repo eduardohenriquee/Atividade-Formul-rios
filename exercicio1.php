d2d5ce84a17419edf16c1dcde85f727692197f5e
eduardohenriquee <48135920+eduardohenriquee@users.noreply.github.com>
2019-07-02 10:14:35 -0300

    Create exercicio1.php

---
+++ exercicio1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Exercício 1</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+<div class="quadrado">
+<h1> Escolha a operação bancária </h1>
+<form>
+    <input type="radio" value="saque" name="operacao"> Saque <br><br>
+    <input type="radio" value="versaldo" name="operacao"> Ver Saldo <br><br>
+    <input type="radio" value="extrato" name="operacao"> Extrato <br><br>
+    <input type="radio" value="pagamento" name="operacao"> Pagamento <br><br>
+    <button type="submit" style="border-radius:5px;"> Escolha feita! </button>
+</form>
+</div>
+
+</body>
+
+</html>
